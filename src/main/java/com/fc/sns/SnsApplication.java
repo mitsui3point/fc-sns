7e5cd4ba00f81a3833b7c96f22df4f86bde65a06
mitsui3point <rladbsgh0331@gmail.com>
2023-06-22 00:08:43 +0900

    feat: optimize imports

--- src/main/java/com/fc/sns/SnsApplication.java
+++ src/main/java/com/fc/sns/SnsApplication.java
@@ -1,9 +1,7 @@
 package com.fc.sns;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SnsApplication {
